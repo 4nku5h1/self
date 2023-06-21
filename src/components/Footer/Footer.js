82571a0b5acb46f19b24f0a574c1647fcb717421
ankushshrivastava98 <hackermr37@gmail.com>
2023-06-21 23:04:13 +0530

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,58 @@
+import { motion } from "framer-motion"
 import { ParallaxLayer } from "@react-spring/parallax";
-import About from "./About";
+import { about, services } from "../Services/data/data";
+import socialData from './data.js'
 
-export default function Footer({pageOffset}) {
+
+import twitter from '../../assets/social/twitter.svg';
+export default function Footer({ pageOffset }) {
     return (
         <>
             <ParallaxLayer offset={pageOffset} speed={1}>
-                <About />
+                <div className='page footer'>
+                    <div className="left">
+                        <h2>About us</h2>
+                        <p>{about}</p>
+                        <div className="services">
+                            <h2>Services</h2>
+                            <div>
+                                {services.map((it) => {
+                                    return (<p>{it.title}</p>)
+                                })}
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="right">
+                        <div className="right-container">
+                            <div className='logo'>
+                                <h2>GRAPHIC<span>NIX</span></h2>
+                                <h3>A design studio</h3>
+                            </div>
+                            <div className="contact">
+                                <h2>Follow us</h2>
+                                <div className='social'>
+                                    {socialData.map((item) => {
+                                        return (
+                                            <motion.div
+                                                className="item"
+                                                whileHover={{ scale: 1.2, rotate: 90 }}
+                                                whileTap={{
+                                                    scale: 0.8,
+                                                    rotate: -90,
+                                                    borderRadius: "100%"
+                                                }}
+                                            >
+
+                                                <img src={item.image}></img>
+                                            </motion.div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </ParallaxLayer>
         </>
     )
---
+++ src/components/Footer/data.js
@@ -0,0 +1,33 @@
+import facebook from '../../assets/social/facebook.svg';
+import insta from '../../assets/social/insta.svg';
+import mail from '../../assets/social/mail.svg';
+import linkedin from '../../assets/social/linkedin.svg';
+import twitter from '../../assets/social/twitter.svg';
+const socialdata = [
+    {
+        image: facebook,
+        name: "",
+        url: ""
+    },
+    {
+        image: mail,
+        name: "",
+        url: ""
+    },
+    {
+        image: linkedin,
+        name: "",
+        url: ""
+    },
+    {
+        image: twitter,
+        name: "",
+        url: ""
+    },
+    {
+        image: insta,
+        name: "",
+        url: ""
+    }
+]
+export default socialdata
