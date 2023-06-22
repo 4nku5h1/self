37a690916ad5d9afb241a3dbe5816cbabb59c777
ankushshrivastava98 <hackermr37@gmail.com>
2023-06-22 21:40:21 +0530

    deploy changes

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,12 +1,12 @@
 import InfiniteImageGallary from "../Common/ImageCompare/InfiniteImageGallary/InfiniteImageGallary";
-import { useViewportScroll, motion } from "framer-motion";
+// import { useViewportScroll, motion } from "framer-motion";
 export default function Clients() {
-    const { scrollYProgress } = useViewportScroll()
-    const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+    // const { scrollYProgress } = useViewportScroll()
+    // const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
     return (
         <div className='page clients'>
             <h2>TRUSTED CLIENTS </h2>
-            <motion.div
+            {/* <motion.div
                 style={{ scale }}
             >
                 <motion.div
@@ -16,7 +16,7 @@ export default function Clients() {
                 >
                     <h1>asdfhj</h1>
                 </motion.div>
-            </motion.div>
+            </motion.div> */}
         </div>
     )
 }
