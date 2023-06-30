05c9d1ca5bc5ca20ccee4f64ac53d24a584e6ff6
ankushshrivastava98 <hackermr37@gmail.com>
2023-06-30 22:16:28 +0530

    asdf

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -4,21 +4,27 @@ import ClientsData from "./data";
 export default function Client({ id }) {
     const images = ClientsData
     return (
-        <motion.div className='page content flex-center' id={id}>
-            <div className='text-content text-heading'>
-                <h1>TRUSTED CLIENTS</h1>
+        <div className="client-container">
+            <div className='text-content text-heading  client-heading page'>
+                <h1>TRUSTED </h1>
+                <h1>CLIENTS</h1>
             </div>
-            <motion.div className="gallary flex-center">
-                {images.map((image) => {
-                    return (
-                        <div className="image-wrapper">
-                            <img src={image} />
-                        </div>
-                    )
-                })}
+            <motion.div className='page client flex-center' id={id}>
+
+
+                <motion.div className="gallary flex-center">
+                    {images.map((image) => {
+                        return (
+                            <div className="image-wrapper">
+                                <img src={image} />
+                            </div>
+                        )
+                    })}
+                </motion.div>
+
+
             </motion.div>
+        </div>
 
-
-        </motion.div>
     )
 }
