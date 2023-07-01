987eb35f35f40bb86beb2f5ca46384585c19130b
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-02 03:12:32 +0530

    asdf

--- src/components/Common/StickyHorizontalGallary/StickyHorizontalGallary.js
+++ src/components/Common/StickyHorizontalGallary/StickyHorizontalGallary.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
-import b1 from '../../../assets/parallax/b3.webp'
-import b2 from '../../../assets/parallax/b3.webp'
-import b3 from '../../../assets/parallax/b3.webp'
 
 import './styles.scss'
+import i1 from '../../../assets/horizontalgallary/1.webp'
+import i2 from '../../../assets/horizontalgallary/2.webp'
+import i3 from '../../../assets/horizontalgallary/3.webp'
+import i4 from '../../../assets/horizontalgallary/4.webp'
+import i5 from '../../../assets/horizontalgallary/5.webp'
 
 export default function StickyHorizontalGallary() {
     useEffect(() => {
@@ -39,12 +41,22 @@ export default function StickyHorizontalGallary() {
                 <div class="sticky-parent">
                     <div class="sticky">
                         <div class="horizontal">
-                            <div class="dim" >sdfsdfgh</div>
-                            <div class="dim" >sdfg</div>
-                            <div class="dim">sdfg</div>
-                            <div class="dim" >sdfg</div>
-                            <div class="dim" ></div>
-                            <div class="dim" >adsfgklkpj</div>
+                            <div class="item-card" >
+                                <img src={i1} />
+                            </div>
+                            <div class="item-card" >
+
+                                <img src={i2} />
+                            </div>
+                            <div class="item-card">
+                                <img src={i3} />
+                            </div>
+                            <div class="item-card" >
+                                <img src={i4} />
+                            </div>
+                            <div class="item-card" >
+                                <img src={i5} />
+                            </div>
                         </div>
                     </div>
                 </div>
