7cc5ee9e4a22c046e71de1bdafec6ebc57116764
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-02 04:18:34 +0530

    asdf

--- src/components/Common/StickyHorizontalGallary/StickyHorizontalGallary.js
+++ src/components/Common/StickyHorizontalGallary/StickyHorizontalGallary.js
@@ -27,20 +27,21 @@ export default function StickyHorizontalGallary({ data }) {
             }
         }
     }, [])
-    console.log(`${data.length}vh`)
     return (
-        <div>
-            <div className='text-content text-heading'>
-                <h1>OUR SERVICES</h1>
+        <div className='horizontal-gallary'>
+            <div className='title'>
+                <h1>Our Services</h1>
             </div>
             <div className='sticky-image-gallary'>
-                <div class="sticky-parent" style={{height:`${data.length * 100}vh`}}>
+                <div class="sticky-parent" style={{height:`${data.length * 70}vh`}}>
                     <div class="sticky">
-                        <h1>adkff</h1>
                         <div class="horizontal">
                             {data.map((e) => {
                                 return (
                                     <div class="item-card" >
+                                        <div className='card-foreground'>
+                                            <p>{e.title}</p>
+                                        </div>
                                         <img src={e.image} />
                                         <h4>{e.title}</h4>
                                     </div>
