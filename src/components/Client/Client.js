840d343983f6ad3487124be29d6cf262b42f42bc
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-08 02:26:46 +0530

    content-about

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,11 +1,14 @@
-import './styles.scss'
-import clientNames from "./data";
 import AnimatedItems from "../Common/Motion/AnimatedItems";
+import clientNames from "./data";
+
+import './styles.scss'
 export default function Client({ uid }) {
     return (
-        <div className="page-text  client-container page" id={uid}>
-            <h3 className='center-small-capital-heading'>TRUSTED CLIENTS</h3>
-            <AnimatedItems className={'client-item'} animate={true} data={clientNames} child={(item) => <span>{item}</span>} />
+        <div className="page-text  client page" id={uid}>
+            <div className='container'>
+                <h3 className='center-small-capital-heading'>TRUSTED CLIENTS</h3>
+                <AnimatedItems  animate={true} data={clientNames} child={(item) => <span>{item}</span>} />
+            </div>
         </div>
 
     )
