90c6c5536d086c8da886a51fd6bd0bdd2b5a476a
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-08 00:15:01 +0530

    service-mobile

--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -13,19 +13,21 @@ export function Service() {
     return (
         <>
 
-            <ServiceTitle appContext={appContext} data={services}/>
+            <ServiceTitle appContext={appContext} data={services} />
             {services.map((data, index) => {
                 const { title, description, imageBefore, imageAfter } = data;
                 const id = serviceIds[index]
                 return (
                     <div id={id} className='page service'>
-                        <div className='image'>
-                            <ImageCompare imageBefore={imageBefore} imageAfter={imageAfter} scrollTo={0} />
-                        </div>
-                        <div className='text-content'>
-                            <h3>{title}</h3>
-                            <p>{description}</p>
-                            <button className='button'>Explore more</button>
+                        <div className='service-container'>
+                            <div className='image'>
+                                <ImageCompare imageBefore={imageBefore} imageAfter={imageAfter} scrollTo={0} />
+                            </div>
+                            <div className='text-content'>
+                                <h3>{title}</h3>
+                                <p>{description}</p>
+                                <button className='button'>Explore more</button>
+                            </div>
                         </div>
                     </div>
 
