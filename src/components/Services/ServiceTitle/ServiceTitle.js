9a44b3f25558f214fdf5c58d54ac0ba779062dd4
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-08 00:59:23 +0530

    service-mobile-final

--- src/components/Services/ServiceTitle/ServiceTitle.js
+++ src/components/Services/ServiceTitle/ServiceTitle.js
@@ -1,8 +1,6 @@
 import { scrollToId } from '../../Common/helper';
 import AnimatedItems from '../../Common/Motion/AnimatedItems';
 
-import './styles.scss';
-
 export function ServiceTitle({ appContext, data }) {
 
     const isCurrentPage = ["SERVICE-TITLE", "SERVICE-1"].includes(appContext.state?.pageConfig?.id);
@@ -19,13 +17,15 @@ export function ServiceTitle({ appContext, data }) {
         )
     }
     return (
-        <div className='page-text service-title themer' id="SERVICE-TITLE">
-            <h2>Our Services</h2>
-            <p>Graphicnix boasts over 10,000 accomplished projects, accompanied by the esteemed recognition of winning the German Editing Award for our exceptional editing. Partnering with us means collaborating with a dedicated team of professionals who are passionate about driving your success.</p>
-            <h5>Image Editing</h5>
-            <AnimatedItems animate={isCurrentPage} data={data} child={ItemImageEditing} />
-            <h5>Video Editing</h5>
-            <AnimatedItems animate={isCurrentPage} data={['video editing', 'vfx', 'sound fx', 'compositing', 'motion graphics', 'animation',]} child={ItemVideoEditing} />
+        <div className='page service themer service-title' id="SERVICE-TITLE">
+            <div className='service-container'>
+                <h2 className='heading-h2'>Our Services</h2>
+                <p className='paragraph'>Graphicnix boasts over 10,000 accomplished projects, accompanied by the esteemed recognition of winning the German Editing Award for our exceptional editing. Partnering with us means collaborating with a dedicated team of professionals who are passionate about driving your success.</p>
+                <h5 className='heading-h5'>Image Editing</h5>
+                <AnimatedItems animate={isCurrentPage} data={data} child={ItemImageEditing} />
+                <h5 className='heading-h5'>Video Editing</h5>
+                <AnimatedItems animate={isCurrentPage} data={['video editing', 'vfx', 'sound fx', 'compositing', 'motion graphics', 'animation',]} child={ItemVideoEditing} />
+            </div>
         </div>
     )
 }
