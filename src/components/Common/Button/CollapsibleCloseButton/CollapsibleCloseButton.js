c241bab0b2cab79b2fef50480826519ff2c293e6
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-08 16:34:30 +0530

    footer-s

--- src/components/Common/Button/CollapsibleCloseButton/CollapsibleCloseButton.js
+++ src/components/Common/Button/CollapsibleCloseButton/CollapsibleCloseButton.js
@@ -1,6 +1,8 @@
 import './styles.scss';
 export default function CollapsibleCloseButton({ isOpen }) {
     return (
-        <div className={`hamburger ${isOpen?'active':''}`}><span></span></div>
+        <div className='hamburger-container'>
+            <div className={`hamburger ${isOpen ? 'active' : ''}`}><span></span></div>
+        </div>
     )
 }
