21190aa96bd2c61c98d0de984e475af6b369ea45
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-09 19:01:56 +0530

    footer

--- src/components/Services/ServiceLanding/ServiceLanding.js
+++ src/components/Services/ServiceLanding/ServiceLanding.js
@@ -4,15 +4,14 @@ import AnimatedItems from '../../Common/Motion/AnimatedItems';
 import './styles.scss'
 
 export default function ServiceTitle({ appContext, data }) {
-
-    const isCurrentPage = ["SERVICE-LANDING", "SERVICE-1"].includes(appContext.state?.pageConfig?.id);
+    // const isCurrentPage = ["SERVICE-LANDING", "SERVICE-1"].includes(appContext.state?.pageConfig?.id);
     const ItemImageEditing = (item, index) => {
         return (
             <div onClick={() => {
                 scrollToId(`SERVICE-${index + 1}`)
             }}>
                 <h2 c>{item.title}</h2>
-                <p>4X HONORABLE MENTION</p>
+                <p>{item.descriptionOverview}</p>
             </div>
         )
     }
@@ -20,20 +19,19 @@ export default function ServiceTitle({ appContext, data }) {
         return (
             <div>
                 <h2>{item}</h2>
-                <p>4X HONORABLE MENTION</p>
+                <p style={{opacity:0, height:0}}>GRAPHICNIX PREMIUM SERVICE</p>
             </div>
         )
     }
     return (
         <div className='page service themer service-landing' id="SERVICE-LANDING">
-            {/* https://www.sublimio.com/about/ */}
             <div className='container'>
                 <h2 className='heading-h2'>Services</h2>
                 <p className='paragraph'>Graphicnix boasts over 10,000 accomplished projects, accompanied by the esteemed recognition of winning the German Editing Award for our exceptional editing. Partnering with us means collaborating with a dedicated team of professionals who are passionate about driving your success.</p>
                 <h5 className='small-capital-heading'>Image Editing</h5>
-                <AnimatedItems className="service-card" animate={isCurrentPage} data={data} child={ItemImageEditing} />
+                <AnimatedItems className="service-card" animate={true} data={data} child={ItemImageEditing} />
                 <h5 className='small-capital-heading'>Video Editing</h5>
-                <AnimatedItems className="service-card" animate={isCurrentPage} data={['Video Editing', 'VFX', 'Sound-FX', 'Compositing', 'Motion Graphics', 'Animation',]} child={ItemVideoEditing} />
+                <AnimatedItems className="service-card" animate={true} data={['Video Editing', 'VFX', 'Sound-FX', 'Compositing', 'Motion Graphics', 'Animation',]} child={ItemVideoEditing} />
             </div>
         </div>
     )
--- src/components/Services/data/data.js
+++ src/components/Services/data/data.js
@@ -4,7 +4,6 @@ import productAfter from '../../../assets/services/productAfter.jpg'
 import virtualStagingBefore from '../../../assets/services/virtualStagingBefore.jpg'
 import virtualStagingAfter from '../../../assets/services/virtualStagingAfter.jpg'
 
-
 import day2DuskBefore from '../../../assets/services/day2DuskBefore.jpg'
 import day2DuskAfter from '../../../assets/services/day2DuskAfter.jpg'
 
@@ -14,11 +13,10 @@ import imageEnhancementAfter from '../../../assets/services/imageEnhancementAfte
 import itemRemovalBefore from '../../../assets/services/itemRemovalBefore.jpg'
 import itemRemovalAfter from '../../../assets/services/itemRemovalAfter.jpg'
 
-
 import render1 from '../../../assets/services/render1.jpeg'
 
-import i1 from '../../../assets/horizontalgallary/1.webp'
-import i3 from '../../../assets/horizontalgallary/3.webp'
+// import i1 from '../../../assets/horizontalgallary/1.webp'
+// import i3 from '../../../assets/horizontalgallary/3.webp'
 
 export const about = 'Graphicnix is the leading online hub for all things digital. Our global team of professional editing experts are ready to transform your photos into eye-catching professional images. Simply upload your photos and let our professionals take care of the rest. We offer a wide range of services including, photo editing, virtual staging, floor plan redraws, renders, and virtual renovations. The process is fast, easy-to-use and extremely affordable.'
 export const services = [
@@ -26,44 +24,44 @@ export const services = [
         id: 'services',
         title: 'Product Editing',
         description: 'Professional jewelry photo retouching attracts potential buyers and presents precious gemstones in the most appealing way. Necklaces, earrings, rings and other jewelry will shine after photoshopping.',
+        descriptionOverview:'Edit Product/ecommerce photos and make them Amazon or catalog-worthy. (e.g: Shoes, Bags, Cloth, etc.)',
         imageBefore: productBefore,
         imageAfter: productAfter,
-        titleImage: productAfter,
 
     },
     {
         id: 'service1',
         title: 'Image Enhancement',
         description: 'Perfect for real estate photo editing so you can present your listing with top-notch, retouched professional-looking photos.',
+        descriptionOverview:'Sky Replacement, White Balancing, Sharpening, Lens correction, Tone Adjustment, Lawn Enhancement, Flash &  Reflection Removal, etc',
         imageBefore: imageEnhancementBefore,
         imageAfter: imageEnhancementAfter,
         process: {
             title: '10 Step Enhancement Process',
-            list: ['White Balancing, Sharpening ', 'Remove Minor Blemishes', 'Lens Distortion Removal', 'Brightness Contrast & Tone Adjustment', 'Sky Replacement', 'Lawn Enhancement – Repair or Replace', 'Ocean/River Water Enhancement', 'TV Screen Replacement', 'Flash & Photographer’s Reflection Removal', 'HDR Bracketing with Indoor Window Replacement']
+            list: ['White Balancing, Sharpening', 'Remove Minor Blemishes', 'Lens Distortion Removal', 'Brightness Contrast & Tone Adjustment', 'Sky Replacement', 'Lawn Enhancement – Repair or Replace', 'Ocean/River Water Enhancement', 'TV Screen Replacement', 'Flash & Photographer’s Reflection Removal', 'HDR Bracketing with Indoor Window Replacement']
         },
-        titleImage: i1,
 
     },
     {
         id: 'service2',
         title: 'Virtual Staging',
         description: 'Our team can add furniture to real estate photography to show potential buyers just how versatile the space is.',
+        descriptionOverview:'clutter removal, interior and exterior virtual renovation/remodeling',
         imageBefore: virtualStagingBefore,
         imageAfter: virtualStagingAfter,
-        titleImage: i3,
 
     },
     {
         id: 'service3',
         title: 'Day to Dusk',
         description: 'Dusk is a beautiful time of the day but is notoriously hard to photograph,We can turn any daytime or gloomy sky into a breathtaking sunset to really make your listing stand out from the rest. Our photo retouching service will take your exterior property photo from drab to fab by replacing your sky with a beautiful dusk setting.',
+        descriptionOverview:'Sky Replacement, Correct light & shadows',
         imageBefore: day2DuskBefore,
         imageAfter: day2DuskAfter,
         process: {
             title: '10 Step Enhancement Process',
             list: []
         },
-        titleImage: day2DuskBefore,
 
 
     },
@@ -71,17 +69,17 @@ export const services = [
         id: 'service4',
         title: 'Item Removal',
         description: 'Our photo editing experts can remove unwanted items from your photos.',
+        descriptionOverview:'Item Removal without leaving any trace of editing.',
         imageBefore: itemRemovalBefore,
         imageAfter: itemRemovalAfter,
-        titleImage: itemRemovalAfter,
 
     },
     {
         id: 'service6',
         title: 'Renders and Floor Plan',
-        description: 'Bring plans to life with our render service available for a range of plans and designs.',
+        description: 'Our talented professionals use their skills and recent technological advancements to produce professional 3D renderings that allow clients to see every part of the interior/Exterior',
+        descriptionOverview:'3D rendering, 2D/3D floor plans, Site plans renders',
         imageBefore: render1,
         imageAfter: render1,
-        titleImage: render1,
     }
 ]
