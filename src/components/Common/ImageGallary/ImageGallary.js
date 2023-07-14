1f4a37a561c9f473ff95859e660236d2dafc6745
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-14 21:53:10 +0530

    gallary

--- src/components/Common/ImageGallary/ImageGallary.js
+++ src/components/Common/ImageGallary/ImageGallary.js
@@ -1,15 +1,21 @@
 import b1 from '../../../assets/parallax/b3.webp'
-
+import b2 from '../../../assets/horizontalgallary/1.webp'
+import b3 from '../../../assets/horizontalgallary/2.webp'
+import b4 from '../../../assets/horizontalgallary/4.webp'
+import b5 from '../../../assets/horizontalgallary/5.webp'
 import './styles.scss';
 
 export default function ImageGallary({ id, data }) {
     data = {
-        row1: [b1, b1, , b1, b1],
-        row2: [b1, b1, , b1, b1],
+        row1: [b1, b2, b5, b1],
+        row2: [b3, b4, b5, b1],
+    }
+    function handleScroll(e){
+        console.log(e)
     }
     return (
         <div className='page'>
-            <div id={id} className='image-gallary'>
+            <div id={id} className='image-gallary' onScroll={handleScroll}>
                 <div className='image-gallary-container'>
                     {Object.values(data).map((row) => {
                         return (
@@ -24,9 +30,11 @@ export default function ImageGallary({ id, data }) {
                         )
                     })}
                 </div>
+                
+                <button  className='button btn-view-gallery-mobile'>View Gallery</button>
                 <div className='image-gallary-foreground' >
                     <h2>Want to see more?</h2>
-                    <button  className='button'>View Gallary</button>
+                    <button  className='button'>View Gallery</button>
                 </div>
             </div>
 
