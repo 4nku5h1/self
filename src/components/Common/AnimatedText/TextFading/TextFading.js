cefe4b0c5466efe81b6c173707060a95be455f83
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-15 22:28:44 +0530

    huge changes

---
+++ src/components/Common/AnimatedText/TextFading/TextFading.js
@@ -0,0 +1,21 @@
+import './styles.scss'
+export default function TextFading() {
+    return (
+        <div className='text-fading'>
+            {/* <div id="title">
+                GRAVITY
+            </div> */}
+            <div id="subtitle">
+                <span>
+                    DONT
+                </span>
+                <span>
+                    LET
+                </span>
+                <span>
+                    GO
+                </span>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Common/Galaxy/Galaxy.js
@@ -0,0 +1,15 @@
+import './styles.scss'
+export default function Galaxy() {
+    return (
+        <div className='galaxy'>
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            {/* <div id="horizon">
+                <div class="glow"></div>
+            </div>
+            <div id="earth"></div> */}
+           
+        </div>
+    )
+}
