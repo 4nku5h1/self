b82f62bba1665035b7f2693212022f48556e746a
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-29 14:10:47 +0530

    react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,3 +1,5 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home/Home';
 
 import '../css/styles.scss'
@@ -5,7 +7,12 @@ import '../css/styles.scss'
 export default function MainComponent() {
     return (
         <div className='main'>
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/gallary" element={<Home />} /> */}
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
