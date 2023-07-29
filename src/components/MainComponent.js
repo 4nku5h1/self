8a47e97ba1349edfdf6eac992648b48364b7517f
ankushshrivastava98 <hackermr37@gmail.com>
2023-07-29 15:40:39 +0530

    email mailer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,9 @@ export default function MainComponent() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 {/* <Route path="/gallary" element={<Home />} /> */}
+                
+                <Route path="*" element={<h1>Page Not Found</h1>}
+                />
             </Routes>
         </div>
     )
